91442ab700e7845c717a6fc194474f74fb2bc056
Franz Osco <franzosco@gmail.com>
2023-11-08 10:48:58 -0500

    add pages

---
+++ webpack.web.js
@@ -0,0 +1,13 @@
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin"),
+  common = require("./webpack.common.js"),
+  merge = require("webpack-merge"),
+  path = require("path");
+
+module.exports = merge(common, {
+  output: {
+    filename: "[name].bundle.js",
+    path: path.join(__dirname, "static"),
+    publicPath: "./",
+  },
+  plugins: [new UglifyJSPlugin()],
+});
